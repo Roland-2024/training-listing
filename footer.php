0cccffc7cfe1bfb7a2b8da48bc2273bcd6ebee66
roland <rolandaga.al@gmail.com>
2024-11-06 16:13:36 +0100

    footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <p>&copy; Copyright <?php echo date("Y"); ?>, Trajnime
+                <p>&copy; Copyright <?php echo date("Y"); ?>, <?php echo esc_html(get_bloginfo('name')) ?>
                     <!-- | Design: <a href="https://rolandaga.com" rel="sponsored" target="_parent">R.Aga</a> -->
                 </p>
             </div>
